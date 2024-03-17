6bce477409b1c78155f999b0d1238ff1f3ea31d6
mairarafiq301 <mairarafiq301@gmail.com>
2024-03-17 04:41:05 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer" ;
+
+import chalk from "chalk"  ;
+
+let  answers = await inquirer.prompt({
+    name: "age",
+    type: "number",
+    message: "Enter your age:"
+});
+console.log(chalk.red("Inshallah , after " + chalk.green( 60 - answers.age ) +  " years you will be 60 years old. "));
+console.log(chalk.yellow("My name is MAIRA KANWAL"));
+console.log(chalk.blue("My name is "  + chalk.blue.bold("Maira Kanwal")));
+console.log(chalk.green ("I  have done " + chalk.green.bold("bechelors of commerce.")));
+
+
+
